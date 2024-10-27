f16438657f785242012a7799e37a3538a7cb3fdb
DuckDemos <leonel078@gmail.com>
2024-10-27 09:29:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/**Menu */
+((d)=> {
+    const $btnmenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+    $btnmenu.addEventListener("click", e=> {
+        $btnmenu.firstElementChild.classList.toggle("none");
+        $btnmenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("in-active");
+    });
+
+    d.addEventListener("click", e=> {
+        if(!e.target.matches(".menu")) return false;
+
+        $btnmenu.firstElementChild.classList.remove("none");
+        $btnmenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
+    })
+})(document)
